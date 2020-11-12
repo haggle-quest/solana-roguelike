85c6dfae607f3ba1aad50313001372f23c8c9642
mr. mac <geshurytom@gmail.com>
2020-11-12 01:33:20 -0700

    More code for don

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const { includes } = require("lodash");
 const {
   default: connectToSolana,
 } = require("./client/src/utils/connectToSolana");
-const { PublicKey } = require("@solana/web3.js");
-// Sentry.init({
-//   dsn: 'https://1c2161c3355341099251260800b07f16@sentry.io/5188534',
-// })
+const { PublicKey, Account } = require("@solana/web3.js");
+import { Token } from "@solana/spl-token";
+
+require("dotenv");
 
 import * as BufferLayout from "buffer-layout";
+import { createAccount } from "./getAccount";
 
 const getData = {
   programId: "7qRP43DEvp1NRGXu76bMzbj5rzYmrJi1AYrXueyYMYHh",
@@ -49,8 +50,6 @@ app.get("/fetch-organizations", async (req, res) => {
     BufferLayout.u32("count2"),
   ]);
 
-  const counts = accountDataLayout.decode(data);
-
   const fetchList = await axios.get(
     `https://api.github.com/repos/${owner}/${repo}/issues?state=all`,
   );
@@ -67,22 +66,42 @@ app.get("/fetch-organizations", async (req, res) => {
   });
 
   console.log(fetchList.data.length, filteredList.length);
-  //   const accountDataLayout = BufferLayout.struct([
-  //     BufferLayout.u32("vc1"),
-  //     BufferLayout.u32("vc2"),
-  //   ]);
 
   res.json("test");
 });
 
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "localhost:3000"); // update to match the domain you will make the request from
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept",
-//   );
-//   next();
-// });
+const TOKEN_PROGRAM_ID = new PublicKey(getData.programId);
+
+export const createTokenAccount = async (account) => {
+  const tokenPublicKey = new PublicKey(account._keypair.publicKey);
+
+  const account2 = await createAccount(process.env.PRIVATE_KEY);
+
+  const connection = await connectToSolana();
+
+  const poop = await connection.getBalance(account2.publicKey);
+  console.log(poop);
+
+  const token = await new Token(
+    connection,
+    tokenPublicKey,
+    TOKEN_PROGRAM_ID,
+    account2,
+  );
+
+  console.log(token);
+  return (await token.createAccount(tokenPublicKey)).toString();
+};
+
+app.get("/create-account", async (req, res) => {
+  const newAccount = await new Account();
+
+  const what = await createTokenAccount(newAccount);
+  console.log(what);
+  res.send(newAccount);
+});
+
+app.put("update-account");
 
 app.use(function (err, req, res, next) {
   console.log(err);
